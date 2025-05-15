a9e037653da96aadbe2ed7cf89881f631d3d2c31
marleypm16 <yarlempm16@gmail.com>
2025-05-15 10:30:47 -0300

    feat : add rota empresa

---
+++ app/routes/empresa.routes.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify";
+import { EmpresaController } from "../controller/empresaController";
+
+
+const empresaRoutes = async (app: FastifyInstance) => {
+    app.get("/empresas",EmpresaController.getEmpresas)
+    app.post("/empresas",EmpresaController.createEmpresa)
+}
+
+export default empresaRoutes;
