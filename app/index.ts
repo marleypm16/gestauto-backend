7b01024d8cb79c76e393741a25daed82339890f4
marleypm16 <yarlempm16@gmail.com>
2025-05-15 10:24:56 -0300

    feat : add rotas de empresas

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,7 @@
 // src/index.js
 import Fastify from 'fastify'
 import userRoutes from './routes/user.routes'
+import empresaRoutes from './routes/empresa.routes'
 const app = Fastify()
 
 
@@ -8,6 +9,7 @@ app.get('/', async (request, reply) => {
   return { hello: 'world' }})
 
 app.register(userRoutes)
+app.register(empresaRoutes)
 
 
 
