80845287cf553a5dfe4af606aa8250c92c38a89e
marleypm16 <yarlempm16@gmail.com>
2025-05-16 08:53:53 -0300

    create : login model

---
+++ app/models/loginModel.ts
@@ -0,0 +1,8 @@
+import {z} from 'zod';
+
+export const loginModel = z.object({
+    email: z.string().email({message:"Digite um e-mail válido"}).min(1,{message:"E-mail é obrigatório"}),
+    senha: z.string().min(8,{message:"A senha deve ter no mínimo 8 caracteres"}).regex(/[a-zA-Z]/, 'A senha deve conter pelo menos uma letra')
+    .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
+    .regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial'),
+})
