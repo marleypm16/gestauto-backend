cd831f42b66572b4cda720636b277bd87b33038c
marleypm16 <yarlempm16@gmail.com>
2025-05-17 12:52:14 -0300

    feat : criando redis plugin

--- app/plugin/redis.ts
+++ app/plugin/redis.ts
@@ -1,8 +1,11 @@
+
 import {createClient} from 'redis';
+import app from '..';
+
 
 export const redisClient = createClient({
-    username: 'default',
-    password: '*******',
+    username: 'admin',
+    password: 'Admin123@',
     socket: {
         host: 'redis-14390.c308.sa-east-1-1.ec2.redns.redis-cloud.com',
         port: 14390
@@ -13,9 +16,14 @@ redisClient.on('error', (err) => {
     console.log('Redis Client Error', err);
 });
 
+
 export async function connectRedis() {
-    await redisClient.connect();
-    console.log('Redis Client Connected');
+    await redisClient.connect().then(() => {
+        console.log('Redis conectado');}).catch((err) => {
+                    console.log('Erro ao conectar no Redis', err);
+
+            return
+        })
 }
 
 
