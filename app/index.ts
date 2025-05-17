b535a3a719bacf2204433c5f0238cda14fb4778c
marleypm16 <yarlempm16@gmail.com>
2025-05-17 12:58:16 -0300

    feat : inserindo rotas

--- app/index.ts
+++ app/index.ts
@@ -2,14 +2,19 @@
 import Fastify from 'fastify'
 import userRoutes from './routes/user.routes'
 import empresaRoutes from './routes/empresa.routes'
+import jwtPlugin from './plugin/jwt'
+import authRoutes from './routes/auth.routes'
 const app = Fastify()
 
 
 app.get('/', async (request, reply) => {
   return { hello: 'world' }})
 
+app.register(jwtPlugin)
+app.register(authRoutes)
 app.register(userRoutes)
 app.register(empresaRoutes)
+
 
 
 
