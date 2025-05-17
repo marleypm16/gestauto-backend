d68e027680109a87c4c70012d29025d16334feef
marleypm16 <yarlempm16@gmail.com>
2025-05-17 18:18:19 -0300

    remove : import inutil

--- app/routes/empresa.routes.ts
+++ app/routes/empresa.routes.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { EmpresaController } from "../controller/empresaController";
 
-
 const empresaRoutes = async (app: FastifyInstance) => {
     app.get("/empresas",EmpresaController.getEmpresas)
     app.post("/empresas",EmpresaController.createEmpresa)
--- app/routes/user.routes.ts
+++ app/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import app from "..";
 import { UsuarioController } from "../controller/usuarioController";
+import authMiddleware from "../middleware/middleware";
 
 const userRoutes = async (app:FastifyInstance) =>{
     app.get("/usuarios", UsuarioController.getUsuarios);
