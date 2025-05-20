04f109f8c84007d9590eb72c7d46264e14babf6e
marleypm16 <yarlempm16@gmail.com>
2025-05-20 16:19:42 -0300

    feat : criacao do serviço de criar senha

--- app/services/novaSenhaService.ts
+++ app/services/novaSenhaService.ts
@@ -3,17 +3,26 @@ import usuarioSchema from "../schemas/usuarioSchema";
 import bcrypt from "bcryptjs";
 
 export class NovaSenhaService{
-    async criarNovaSenha(email:string,senha:string,otp:string){
+    async criarNovaSenha(email:string,novaSenha:string,otp:string){
+        //validar se o otp existe no redis
         const resultado = await redisClient.get(`otp:${otp}`)
-        const senhaNovaCriptografada = await bcrypt.hash(senha, 12);
+        
         if(!resultado){
             console.log("OTP Inválida")
         }
+        //criptografar a nova senha
+        const senhaNovaCriptografada = await bcrypt.hash(novaSenha, 12);
+        //validar se o usuário existe
+        const usuario = await usuarioSchema.findOne({ email });
+        if (!usuario) {
+            throw new Error("Usuário não encontrado");
+        }
+        //validar se a senha atual é igual a nova senha
 
-        await usuarioSchema.findOneAndUpdate({
-            email
-        },{
-            $set:{senha : senhaNovaCriptografada }
-        })
+        if(bcrypt.compareSync(novaSenha, usuario.senha)){
+            throw new Error("A nova senha não pode ser igual a senha atual")
+        }
+        usuario.senha = senhaNovaCriptografada;
+        await usuario.save()
     }
 }
