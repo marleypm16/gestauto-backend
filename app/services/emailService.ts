da151fb1dc01ccf50906bb0e69c86d56821944a1
marleypm16 <yarlempm16@gmail.com>
2025-05-20 14:44:38 -0300

    feat : criando serviço de email

---
+++ app/services/emailService.ts
@@ -0,0 +1,14 @@
+import { redisClient } from "../plugin/redis";
+import sendEmailSes from "../plugin/ses";
+
+export class EmailService{
+   static async  sendEmail(to: string,otp:number): Promise<void> {       
+        await redisClient.set(`otp:${otp}`, otp, { EX: 300 }).catch((error) => {
+            console.error('Erro ao armazenar OTP no Redis:', error);
+            throw new Error('Erro ao armazenar OTP no Redis');
+        })
+
+        await sendEmailSes(to,otp)
+        
+    }
+}
