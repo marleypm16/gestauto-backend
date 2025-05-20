bdc2ab226db0cbe3e9e32499554fdfb9084d9f54
marleypm16 <yarlempm16@gmail.com>
2025-05-20 16:19:27 -0300

    feat : adicionando email para novasenhamodel

--- app/models/novaSenhaModel.ts
+++ app/models/novaSenhaModel.ts
@@ -3,7 +3,8 @@ import {string, z} from "zod"
 const novaSenhaModel = z.object({
     novaSenha: string().min(1,{message: "Digite uma nova senha"}).regex(/[a-zA-Z]/, 'A senha deve conter pelo menos uma letra')
     .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
-    .regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial')
+    .regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial'),
+    email: z.string().email({message:"Digite um e-mail válido"}).min(1,{message:"E-mail é obrigatório"})
 })
 
 export default novaSenhaModel
