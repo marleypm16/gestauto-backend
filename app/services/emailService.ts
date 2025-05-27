b5c11ce543c3073276e13eed5f844db05a1dfd2f
marleypm16 <yarlempm16@gmail.com>
2025-05-27 12:02:44 -0300

    feat : criando servico para verificar email

--- app/services/emailService.ts
+++ app/services/emailService.ts
@@ -1,5 +1,6 @@
 import { redisClient } from "../plugin/redis";
 import sendEmailSes from "../plugin/ses";
+import usuarioSchema from "../schemas/usuarioSchema";
 
 export class EmailService{
    static async  sendEmail(to: string,otp:number): Promise<void> {       
@@ -11,4 +12,29 @@ export class EmailService{
         await sendEmailSes(to,otp)
         
     }
+    static async verifyEmail(email: string, otp: number) {
+        const storedOtp = await redisClient.get(`otp:${email}`);
+        if (storedOtp && parseInt(storedOtp) === otp) {
+            const usuario = await usuarioSchema.findOne({
+                email
+            })
+            if (!usuario) {
+                throw new Error('Usuário não encontrado');
+            }
+            await redisClient.del(`otp:${email}`);
+            await usuario.updateOne({
+                email,
+                $set:{
+                    emailVerificado: true
+                }
+            })
+            return {
+                id: usuario._id,
+                nome: usuario.nomeCompleto,
+                email: usuario.email
+            };
+        }
+        return null;
+    }
+
 }
