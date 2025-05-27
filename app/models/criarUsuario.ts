8f7360bbaba9206935e46b7d2c3c38d486de6b4a
marleypm16 <yarlempm16@gmail.com>
2025-05-27 11:07:29 -0300

    feat : melhorando codigo

--- app/models/criarUsuario.ts
+++ app/models/criarUsuario.ts
@@ -1,10 +1,14 @@
 import {z} from 'zod';
+import { criarEmpresaModel } from './criarEmpresa';
 
 export const criarUsuarioModel = z.object({
-    nomeCompleto: z.string().min(1,{message:"Nome completo é obrigatório"}),
-    email: z.string().email({message:"Digite um e-mail válido"}).min(1,{message:"E-mail é obrigatório"}),
-    senha: z.string().min(8,{message:"A senha deve ter no mínimo 8 caracteres"}).regex(/[a-zA-Z]/, 'A senha deve conter pelo menos uma letra')
-    .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
-    .regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial'),
-    empresaId: z.string().min(1,{message:"ID da empresa é obrigatório"})
-})
+    user:z.object({
+        nomeCompleto: z.string().min(1,{message:"Nome completo é obrigatório"}),
+        email: z.string().email({message:"Digite um e-mail válido"}).min(1,{message:"E-mail é obrigatório"}),
+        senha: z.string().min(8,{message:"A senha deve ter no mínimo 8 caracteres"}).regex(/[a-zA-Z]/, 'A senha deve conter pelo menos uma letra')
+        .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
+        .regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial'),
+    }),
+    company: criarEmpresaModel
+    
+})  
