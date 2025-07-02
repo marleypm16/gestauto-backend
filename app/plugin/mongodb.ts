38e146d5233487367b420b4d5ce77175c7c56ce0
marleypm16 <yarlempm16@gmail.com>
2025-07-02 10:42:09 -0300

    remove mongodb

---
+++ app/plugin/mongodb.ts
@@ -0,0 +1,15 @@
+// import mongoose from "mongoose";
+
+// const connectMongoDb = async() => {
+//     await mongoose.connect(
+//         process.env.DB_URL || 'mongodb://localhost:27017/mydatabase',
+//     ).then((
+//         () => {
+//             console.log('Banco de dados conectado com sucesso');
+//         }
+//     )).catch((error) => {
+//         console.error('Erro ao conectar com banco de dados:', error);
+//         process.exit(1); 
+//     });}
+
+// export default connectMongoDb;
