5d71282d6f57882797dbd610ba6744a4c7142d65
marleypm16 <yarlempm16@gmail.com>
2025-07-02 10:38:57 -0300

    add otp route and cookies

--- app/index.ts
+++ app/index.ts
@@ -4,14 +4,21 @@ import authRoutes from './routes/auth.routes'
 import authenticatedRoutes from './routes/authenticated.routes'
 import emailRoutes from './routes/email.routes'
 import cors from './plugin/cors'
+import otpRoute from './routes/otp.routes'
+import cookiesPlugin from './plugin/cookies'
 const app = Fastify()
 
 
 app.get('/', async (request, reply) => {
   return { hello: 'world' }})
+// Em algum lugar no seu setup de rotas
+
 app.register(cors)
+app.register(cookiesPlugin)
+
 app.register(jwtPlugin)
 app.register(emailRoutes)
+app.register(otpRoute)
 app.register(authRoutes)
 app.register(authenticatedRoutes)
 
