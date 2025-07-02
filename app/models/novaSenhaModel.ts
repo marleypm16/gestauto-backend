ec5277d41f3888c3189a8382c51f7b3d6e895e5e
marleypm16 <yarlempm16@gmail.com>
2025-07-02 10:39:54 -0300

    feat : nova senha model

--- app/models/novaSenhaModel.ts
+++ app/models/novaSenhaModel.ts
@@ -4,7 +4,7 @@ const novaSenhaModel = z.object({
     novaSenha: string().min(1,{message: "Digite uma nova senha"}).regex(/[a-zA-Z]/, 'A senha deve conter pelo menos uma letra')
     .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
     .regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial'),
-    email: z.string().email({message:"Digite um e-mail válido"}).min(1,{message:"E-mail é obrigatório"})
+    email: z.string().email({message:"Digite um e-mail válido"}).min(1,{message:"E-mail é obrigatório"}),
 })
 
 export default novaSenhaModel
